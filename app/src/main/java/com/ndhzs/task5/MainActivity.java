054207670f96afd0edd8f036ac01d47ddf1a8151
985892345 <2767465918@qq.com>
2020-11-28 00:41:04 +0800

    写好了登陆界面

--- app/src/main/java/com/ndhzs/task5/MainActivity.java
+++ app/src/main/java/com/ndhzs/task5/MainActivity.java
@@ -2,7 +2,9 @@ package com.ndhzs.task5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
